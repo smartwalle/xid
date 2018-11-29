d22a81c81e2c1b48a5b1f2d51573dcdd55332557
SmartWalle <smartwalle@gmail.com>
2018-11-29 09:20:39 +0800

    update

--- xid_test.go
+++ xid_test.go
@@ -1,29 +1,17 @@
 package xid
 
 import (
-	"testing"
 	"fmt"
-	"time"
+	"testing"
 )
 
 func TestNewXID(t *testing.T) {
-
-
-	fmt.Println("秒秒（ s）", time.Now().Unix()) //获取当前秒（s）
-	fmt.Println("纳秒（ns）", time.Now().UnixNano())//获取当前纳秒（ns）
-	fmt.Println("微秒（µs）", time.Now().UnixNano()/1e3)//获取当前微秒（µs）
-	fmt.Println("毫秒（ms）", time.Now().UnixNano()/1e6)//将纳秒转换为毫秒（ms）
-	fmt.Println("秒秒（ s）", time.Now().UnixNano()/1e9)//将纳秒转换为秒（s）
-
-	return
-
-
 	fmt.Println(NewXID())
 	fmt.Println(NewMID())
 }
 
 func BenchmarkNewXID(b *testing.B) {
-	for i :=0; i<b.N; i++ {
+	for i := 0; i < b.N; i++ {
 		NewXID()
 	}
 }

31779880f103afb888bacc5ada62601fa8fcdb9d
smartwalle <smartwalle@gmail.com>
2020-03-20 11:44:09 +0800

    update

--- xid.go
+++ xid.go
@@ -7,30 +7,34 @@ import (
 	"time"
 )
 
+// 34 位用于存储时间戳(秒)信息，最大可以存储的值为 17179869183，即 2514-05-30 09:53:03.
+// 8 位用于存储数据节点信息，最大可以存储 255，即可以有 256 个节点.
+// 21 位用于存储每一秒产生的序列号信息，最大可以存储 2097151，即每秒最大可以生产 2097152 个序列号.
+
 const (
-	kSequenceBits uint8 = 22 // 序列号占用的位数
-	kNodeBits     uint8 = 8  // 数据节点占用的位数
+	kSequenceBits uint8 = 21 // 序列号占用的位数
+	kDataNodeBits uint8 = 8  // 数据节点占用的位数
 
-	kMaxSequence int64 = -1 ^ (-1 << kSequenceBits) // 序列号最大值，用于防止溢出
-	kMaxNode     int64 = -1 ^ (-1 << kNodeBits)     // 数据节点最大值，用于防止溢出 0-255
+	kMaxSequence int64 = -1 ^ (-1 << kSequenceBits) // 序列号最大值，存储范围为 0-2097151
+	kMaxDataNode int64 = -1 ^ (-1 << kDataNodeBits) // 数据节点最大值，存储范围为 0-255
 
-	kTimeShift = kNodeBits + kSequenceBits // 时间戳向左的偏移量
-	kNodeShift = kSequenceBits             // 数据节点向左的偏移量
+	kTimeShift     = kDataNodeBits + kSequenceBits // 时间戳向左的偏移量
+	kDataNodeShift = kSequenceBits                 // 数据节点向左的偏移量
 
-	kNodeMask = kMaxNode << kSequenceBits
+	kDataNodeMask = kMaxDataNode << kSequenceBits
 )
 
 var (
-	ErrNodeNotAllowed = errors.New(fmt.Sprintf("xid: node can't be greater than %d or less than 0", kMaxNode))
+	ErrDataNodeNotAllowed = errors.New(fmt.Sprintf("xid: data node can't be greater than %d or less than 0", kMaxDataNode))
 )
 
 type Option func(*XID) error
 
-// WithNode 设置数据节点标识
-func WithNode(node int64) Option {
+// WithDataNode 设置数据节点标识
+func WithDataNode(node int64) Option {
 	return func(x *XID) error {
-		if node < 0 || node > kMaxNode {
-			return ErrNodeNotAllowed
+		if node < 0 || node > kMaxDataNode {
+			return ErrDataNodeNotAllowed
 		}
 		x.node = node
 		return nil
@@ -53,7 +57,7 @@ type XID struct {
 	second     int64 // 上一次生成 id 的时间戳（秒）
 	node       int64 // 数据节点
 	sequence   int64 // 当前秒已经生成的 id 序列号 (从0开始累加)
-	timeOffset int64
+	timeOffset int64 // 时间偏移量
 }
 
 func New(opts ...Option) (*XID, error) {
@@ -92,7 +96,7 @@ func (this *XID) Next() int64 {
 	}
 	this.second = second
 
-	var id = (second-this.timeOffset)<<kTimeShift | (this.node << kNodeShift) | (this.sequence)
+	var id = (second-this.timeOffset)<<kTimeShift | (this.node << kDataNodeShift) | (this.sequence)
 	return id
 }
 
@@ -111,7 +115,7 @@ func Time(s int64) int64 {
 
 // Node 获取 id 的数据节点标识
 func Node(s int64) int64 {
-	return s & kNodeMask >> kNodeShift
+	return s & kDataNodeMask >> kDataNodeShift
 }
 
 //  Sequence 获取 id 的序列号
--- xid_test.go
+++ xid_test.go
@@ -3,11 +3,28 @@ package xid
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestXID_Next(t *testing.T) {
 	for i := 0; i < 100000; i++ {
 		fmt.Println(Next())
+	}
+}
+
+func TestXID_Info(t *testing.T) {
+	var timeOffset = time.Date(2020, time.January, 1, 1, 1, 1, 1, time.UTC)
+
+	var x, _ = New(WithTimeOffset(timeOffset), WithDataNode(1))
+
+	for i := 0; i < 100; i++ {
+		var id = x.Next()
+
+		var createdOn = timeOffset.Add(time.Duration(Time(id)) * time.Second)
+		var sequence = Sequence(id)
+		var node = Node(id)
+
+		t.Log(id, "- 生成时间:", createdOn, "数据节点:", node, "序列号:", sequence)
 	}
 }
 

3a6d75bf0ef568d2052b63381f86eaf3ea951793
smartwalle <smartwalle@gmail.com>
2020-04-30 10:41:24 +0800

    支持使用 zookeeper 来自动维护可用节点

--- zookeeper.go
+++ zookeeper.go
@@ -25,8 +25,24 @@ func WithZookeeper(conn *zk.Conn, key string) Option {
 		}
 	}
 
+	children, _, err := conn.Children(fmt.Sprintf("/smartwalle/xid/%s", key))
+	if err != nil {
+		lock.Unlock()
+		return func(x *XID) error {
+			return err
+		}
+	}
+	var existsNode = make(map[string]struct{}, len(children))
+	for _, child := range children {
+		existsNode[child] = struct{}{}
+	}
+
 	var nNode = -1
-	for i := 0; i < int(kMaxDataNode); i++ {
+	for i := 0; i <= int(kMaxDataNode); i++ {
+		if _, exists := existsNode[fmt.Sprintf("node-%d", i)]; exists {
+			continue
+		}
+
 		var nPath = path.Join(fmt.Sprintf("/smartwalle/xid/%s/node-%d", key, i))
 		exists, _, err := conn.Exists(nPath)
 		if err != nil {

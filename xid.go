2f0c806b6af75b83e6550011102b90120a2177c7
SmartWalle <smartwalle@gmail.com>
2026-06-15 09:28:50 +0800

    refactor: 将ID生成改为无符号整型并移除sync.Once

--- xid.go
+++ xid.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-// 32 位用于存储时间戳(秒)信息，最大可以存储的值为 4294967296，即 2106-02-07 14:28:16.
+// 33 位用于存储时间戳(秒)信息，最大可以存储的值为 8589934591，即 2242-03-16 12:56:31.
 //
 // 10 位用于存储数据节点信息，最大可以存储 1023，即可以有 1024 个节点.
 //
@@ -17,8 +17,8 @@ const (
 	kSequenceBits uint8 = 21 // 序列号占用的位数
 	kDataNodeBits uint8 = 10 // 数据节点占用的位数
 
-	kMaxSequence int64 = -1 ^ (-1 << kSequenceBits) // 序列号最大值，存储范围为 0-2097151
-	kMaxDataNode int64 = -1 ^ (-1 << kDataNodeBits) // 数据节点最大值，存储范围为 0-1023
+	kMaxSequence = ^uint64(0) >> (64 - kSequenceBits) // 序列号最大值，存储范围为 0-2097151
+	kMaxDataNode = ^uint64(0) >> (64 - kDataNodeBits) // 数据节点最大值，存储范围为 0-1023
 
 	kTimeShift     = kDataNodeBits + kSequenceBits // 时间戳向左的偏移量
 	kDataNodeShift = kSequenceBits                 // 数据节点向左的偏移量
@@ -39,10 +39,10 @@ type Option func(*XID) error
 // WithDataNode 设置数据节点标识
 func WithDataNode(node int64) Option {
 	return func(x *XID) error {
-		if node < 0 || node > kMaxDataNode {
+		if node < 0 || uint64(node) > kMaxDataNode {
 			return ErrDataNodeNotAllowed
 		}
-		x.node = node
+		x.node = uint64(node)
 		return nil
 	}
 }
@@ -60,10 +60,10 @@ func WithTimeOffset(t time.Time) Option {
 
 type XID struct {
 	mu         sync.Mutex
-	second     int64 // 上一次生成 id 的时间戳（秒）
-	node       int64 // 数据节点
-	sequence   int64 // 当前秒已经生成的 id 序列号 (从0开始累加)
-	timeOffset int64 // 时间偏移量
+	second     int64  // 上一次生成 id 的时间戳（秒）
+	node       uint64 // 数据节点
+	sequence   uint64 // 当前秒已经生成的 id 序列号 (从0开始累加)
+	timeOffset int64  // 时间偏移量
 }
 
 func New(opts ...Option) (*XID, error) {
@@ -84,20 +84,20 @@ func New(opts ...Option) (*XID, error) {
 }
 
 func (x *XID) DataNode() int64 {
-	return x.node
+	return int64(x.node)
 }
 
 func (x *XID) TimeOffset() int64 {
 	return x.timeOffset
 }
 
-func (x *XID) Next() int64 {
+func (x *XID) Next() uint64 {
 	x.mu.Lock()
 
 	var second = time.Now().Unix()
 	if second < x.second {
 		x.mu.Unlock()
-		return -1
+		return ^uint64(0)
 	}
 
 	if x.second == second {
@@ -112,7 +112,7 @@ func (x *XID) Next() int64 {
 	var sequence = x.sequence
 	x.mu.Unlock()
 
-	var id = (second-x.timeOffset)<<kTimeShift | (x.node << kDataNodeShift) | (sequence)
+	var id = uint64(second-x.timeOffset)<<kTimeShift | (x.node << kDataNodeShift) | sequence
 	return id
 }
 
@@ -125,42 +125,30 @@ func (x *XID) getNextSecond() int64 {
 }
 
 // Time 获取 id 的时间，单位是 second
-func Time(s int64) int64 {
-	return s >> kTimeShift
+func Time(s uint64) int64 {
+	return int64(s >> kTimeShift)
 }
 
 // DataNode 获取 id 的数据节点标识
-func DataNode(s int64) int64 {
-	return s & kDataNodeMask >> kDataNodeShift
+func DataNode(s uint64) int64 {
+	return int64(s & kDataNodeMask >> kDataNodeShift)
 }
 
 // Sequence 获取 id 的序列号
-func Sequence(s int64) int64 {
-	return s & kMaxSequence
+func Sequence(s uint64) int64 {
+	return int64(s & kMaxSequence)
 }
 
 var shared *XID
-var once sync.Once
 
-func Next() int64 {
-	once.Do(func() {
-		shared, _ = New()
-	})
+func Next() uint64 {
 	return shared.Next()
-}
-
-func Init(opts ...Option) (err error) {
-	once.Do(func() {
-		shared, err = New(opts...)
-	})
-
-	if err != nil {
-		once = sync.Once{}
-	}
-
-	return err
 }
 
 func Default() *XID {
 	return shared
+}
+
+func init() {
+	shared, _ = New()
 }
